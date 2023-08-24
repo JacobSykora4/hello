37fa9744fef09b0477e63db7b4987d1f9f3d95ce
Jacob Sykora <jsykora4@huskers.unl.edu>
2023-08-24 11:55:13 -0500

    commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Jacob Sykora
+ *         EMAIL
+ * Date: 2023/08/24
+ *
+ * A simple name printing program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jacob Sykora!\n");
+  printf("computer engineering\n");
+
+  return 0;
+}
